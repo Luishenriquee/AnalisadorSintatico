e82ffaf0cd23e6aced0cc67ddfd3570cc8cd8c1e
Luishenriquee <iluishenriquebr@gmail.com>
2023-06-10 22:56:39 -0300

    Add project files.

---
+++ AnalisadorSintatico/AnalisadorLexico.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AnalisadorSintatico
+{
+    public class AnalisadorLexico
+    {
+        static Dictionary<string, string> PalavraReservada = new Dictionary<string, string>()
+        {
+            { "for", "1" },
+            { "in", "2" },
+            { "range", "3" }
+        };
+
+        static Dictionary<char, string[]> CaractereEspecial = new Dictionary<char, string[]>()
+        {
+            { '(', new string[] { "4", "ABRE PARENT." } },
+            { ')', new string[] { "5", "FECHA PARENT." } }
+        };
+
+        public void ValidarToken(List<string> tokens)
+        {
+            try
+            {
+                foreach (string token in tokens)
+                {
+                    //TODO: Validar Tokens
+                    //TODO: Verificar se é palavra reservada, caractere especial ou Identificador
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("ERRO: " + ex.Message);
+            }
+        }
+
+        public static bool VerificarIdentificador(string palavra)
+        {
+            string identificadorRegex = @"^[a-zA-Z_]\w*$";
+
+            return Regex.IsMatch(palavra, identificadorRegex);
+        }
+    }
+}
---
+++ AnalisadorSintatico/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AnalisadorSintatico
+{
+    class Program
+    {
+        static void Main()
+        {
+            string nomeArquivo = "Arquivo.txt";
+            string caminhoArquivo = @"C:\Users\solda\OneDrive\Área de Trabalho\Analisador";
+
+            string expressao = ConsultarArquivo(nomeArquivo, caminhoArquivo);
+
+            if (expressao is null)
+                Console.WriteLine("Arquivo não encontrado.");
+            else
+            {
+                List<string> palavras = SepararExpressaoEmPalavra(expressao);
+                AnalisadorLexico analisadorLexico = new AnalisadorLexico();
+                analisadorLexico.ValidarToken(palavras);
+            }
+        }
+
+        static string ConsultarArquivo(string nomeArquivo, string caminhoArquivo)
+        {
+            string caminhoCompleto = Path.Combine(caminhoArquivo, nomeArquivo);
+
+            if (File.Exists(caminhoCompleto))
+                return File.ReadAllText(caminhoCompleto);
+            else
+                return null;
+        }
+
+        static List<string> SepararExpressaoEmPalavra(string expressao)
+        {
+            List<string> novasPalavras = new List<string>();
+            List<string> palavras = new List<string>();
+
+            string novaExpressao = expressao.Replace("(", " ( ");
+            novaExpressao = novaExpressao.Replace(",", " , ");
+            novaExpressao = novaExpressao.Replace(")", " ) ");
+
+            palavras = novaExpressao.Split(' ').ToList();
+
+            foreach (string palavra in palavras)
+            {
+                if (!string.IsNullOrEmpty(palavra))
+                    novasPalavras.Add(palavra);
+            }
+
+            return novasPalavras;
+        }
+    }
+}
