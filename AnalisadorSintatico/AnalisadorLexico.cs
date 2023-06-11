f00964115735f188f43f4a209dafbadf2c707941
Luishenriquee <iluishenriquebr@gmail.com>
2023-06-11 12:43:40 -0300

    Adição do método VerificarPalavra que vai definir se um Token é identificador, caractere especial, palavra reservada ou tipo numérico

--- AnalisadorSintatico/AnalisadorLexico.cs
+++ AnalisadorSintatico/AnalisadorLexico.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AnalisadorSintatico
 {
     public class AnalisadorLexico
     {
-        static Dictionary<string, string> PalavraReservada = new Dictionary<string, string>()
+        static Dictionary<string, string> palavraReservada = new Dictionary<string, string>()
         {
             { "for", "1" },
             { "in", "2" },
             { "range", "3" }
         };
 
-        static Dictionary<char, string[]> CaractereEspecial = new Dictionary<char, string[]>()
+        static Dictionary<char, string[]> caractereEspecial = new Dictionary<char, string[]>()
         {
             { '(', new string[] { "4", "ABRE PARENT." } },
-            { ')', new string[] { "5", "FECHA PARENT." } }
+            { ')', new string[] { "5", "FECHA PARENT." } },
+            { ',', new string[] { "6", "VIRGULA." } }
         };
+
+        static List<Tuple<string, string>> tokens = new List<Tuple<string, string>>();
 
         public void ValidarToken(List<string> tokens)
         {
@@ -25,20 +29,81 @@ namespace AnalisadorSintatico
             {
                 foreach (string token in tokens)
                 {
-                    //TODO: Validar Tokens
-                    //TODO: Verificar se é palavra reservada, caractere especial ou Identificador
+                    VerificarPalavra(token);
                 }
+
+                Mostrar();
+                Console.WriteLine("Finalizada Análise Lexica sem Erros.");
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine("ERRO: " + ex.Message);
+            }
+        }
+
+        static void Mostrar()
+        {
+            Console.WriteLine();
+            foreach (var token in tokens.Select((value, index) => new { Value = value, Index = index }))
+            {
+                Console.WriteLine("Token: " + token.Value.Item1 + " | Descrição: " + token.Value.Item2);
+            }
+        }
+
+        public static void VerificarPalavra(string token)
+        {
+            if (VerificarIdentificador(token))
+            {
+                tokens.Add(new Tuple<string, string>(token, "IDENTIFICADOR"));
+            }
+            else if (VerificarCaractereEspecial(token))
+            {
+                tokens.Add(new Tuple<string, string>(token, "CARACTERES ESPECIAIS"));
+            }
+            else if (VerificarPalavraReservada(token))
+            {
+                tokens.Add(new Tuple<string, string>(token, "PALAVRA RESERVADA"));
+            }
+            else if (VerificarNumero(token))
+            {
+                tokens.Add(new Tuple<string, string>(token, "TIPO NUMERICO"));
+            }
+            else
+            {
+                tokens.Add(new Tuple<string, string>(token, "NÃO RECONHECIDO"));
             }
         }
 
         public static bool VerificarIdentificador(string palavra)
         {
             string identificadorRegex = @"^[a-zA-Z_]\w*$";
+            bool verificar = false;
 
+            if (Regex.IsMatch(palavra, identificadorRegex) == true && !VerificarPalavraReservada(palavra) == true)
+                verificar = Regex.IsMatch(palavra, identificadorRegex);
+
+            return verificar;
+        }
+
+        public static bool VerificarPalavraReservada(string palavra)
+        {
+            return palavraReservada.ContainsKey(palavra);
+        }
+
+        public static bool VerificarCaractereEspecial(string palavra)
+        {
+            char[] caracteres = palavra.ToCharArray();
+            bool vereficar = false;
+
+            if (caracteres.Length == 1)
+                vereficar = caractereEspecial.ContainsKey(caracteres[0]);
+
+            return vereficar;
+        }
+
+        public static bool VerificarNumero(string palavra)
+        {
+            string identificadorRegex = @"^-?\d+(\.\d+)?$";
             return Regex.IsMatch(palavra, identificadorRegex);
         }
     }
